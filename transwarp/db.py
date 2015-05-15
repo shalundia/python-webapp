a26125bb2b6e9ec696bb88f3885d9203cf9fc21e
zhang ming <zhangming@zuoshan.local>
2015-05-15 20:29:16 +0800

    second

---
+++ transwarp/db.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#database controller module’
+
+__author__ = 'zhangming'
+
+#数据库引擎对象：
+class _Engine(object):
+    def __init__(self,connect):
+        self._connect=connect
+    def connect(self)
+        return self._connect
+
+engine =None
+
+#持有数据库连接的上下文对象
+class 
+
+
+
+class db(object):
+
+
+
+    def select(sql):
+
+
+    '关键字参数'
+    def create_engine(**user,**password,**database,**host,**port):
+
+
+
+    def update(sql,*args):
+
